76b8fefedeea6329fd9cc46d8f98008dfe6bb83e
Robinson Gonzalez <rgonzalezr0322@gmail.com>
2023-01-18 16:27:12 -0500

    Initial Commit

---
+++ UsersMVVMURLSessionAndGenericsEnvironmentObject/UsersMVVMURLSessionAndGenericsEnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersMVVMURLSessionAndGenericsEnvironmentObjectApp.swift
+//  UsersMVVMURLSessionAndGenericsEnvironmentObject
+//
+//  Created by Robinson Gonzalez on 18/01/23.
+//
+
+import SwiftUI
+
+@main
+struct UsersMVVMURLSessionAndGenericsEnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
